95ed255c37f1ca8a61c75ae8ce1a1cff31983e7e
Leon Strauß <leonstrauss99@gmail.com>
2021-01-29 18:00:47 +0100

    changed to release version

--- src/ISSHConnectConfig.ts
+++
@@ -1,8 +0,0 @@
-import { ConnectConfig } from 'ssh2';
-
-export interface ISSHConnectConfig extends ConnectConfig {
-  host: string;
-  port: number;
-  hop?: ISSHConnectConfig;
-  sock: undefined;
-}
